8b99db912d9d151f86ba4b5d52cebb45c780d9a0
Gavin <gavin@example.com>
2025-12-04 07:10:21 +0800

    fix: Add missing Link import to Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { POSTS, CATEGORIES } from '../data/posts';
 import SEO from '../components/SEO';
 
